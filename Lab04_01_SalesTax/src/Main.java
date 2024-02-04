71f12fcf15a1f4ae6041f0e055e3d6d3f1cf4000
Anshuman <sohananshuman.karri@gmail.com>
2024-02-04 11:24:41 -0500

    Lab4 task1 initial commit

---
+++ Lab04_01_SalesTax/src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double price = 24.99; //user input for price of a purchase
+        double salesTax = price * 0.05; // computes a 5% sales tax using the price given by the user
+        System.out.println("Your sales tax is " + salesTax); // output statement for sales tax
+    }
+}
