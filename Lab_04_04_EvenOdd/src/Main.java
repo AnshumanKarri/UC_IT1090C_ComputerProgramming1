6cc06e2515de5aa5bb50c9bc3c2bbab7e23d6321
Anshuman <sohananshuman.karri@gmail.com>
2024-02-04 11:53:45 -0500

    Lab04 task 4 extra credit initial commit

---
+++ Lab_04_04_EvenOdd/src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int num = 22; // integer give by the user that will be processed
+        if (num % 2 != 0)
+            System.out.println("Your number is odd");
+        else
+            System.out.println("Your number is even");
+    }
+}
