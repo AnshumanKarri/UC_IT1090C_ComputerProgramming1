31e4204829512cd896579be36a044a74c7a2c9a1
Anshuman <sohananshuman.karri@gmail.com>
2024-02-04 11:49:00 -0500

    Lab04 task 3 initial commit

--- Lab04_03_MonthlyCCBalance/src/Main.java
+++ Lab04_03_MonthlyCCBalance/src/Main.java
@@ -5,6 +5,6 @@ public class Main
         double ccBalance = 5000.0; // declaration and initialization of credit card balance variable
         double m1Interest = ccBalance * 0.17; // processing statement to calculate the interest after 1 month
         double m2Interest = ccBalance * 0.17 + m1Interest; // processing statement to calculate the interest after 2 months
-        System.out.println("The interest due after 1 month is " + m1Interest + "\nThe interest due after 2 months is " + m2Interest);
+        System.out.println("The interest due after 1 month is " + m1Interest + "\nThe interest due after 2 months is " + m2Interest); //output statement for the interest due after 1 month and 2 months
     }
 }
