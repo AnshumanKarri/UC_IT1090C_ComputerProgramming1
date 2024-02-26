101dc3f79d701d15da1645eb164067bbc50f7c51
Anshuman <sohananshuman.karri@gmail.com>
2024-02-25 23:50:43 -0500

    lab 7 task 7

---
+++ Lab07_07/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++)//loop for counting rows
+        {
+            for (int j = 0; j < 5; j++)//loop for counting columns
+            {
+                System.out.print("*");//loop for printing out stars
+            }
+            System.out.println();//loops for printing a new line
+        }
+    }
+}
