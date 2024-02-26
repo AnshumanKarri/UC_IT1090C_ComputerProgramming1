509f380f718d95dbef82263edd2c0ef53d524c43
Anshuman <sohananshuman.karri@gmail.com>
2024-02-25 23:44:58 -0500

    lab 7 task 3

--- Lab07_03/src/Main.java
+++ Lab07_03/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i = 0; i <= 18; i += 3)
+        for(int i = 0; i <= 18; i += 3)//loop for counting up by 3
         {
-            System.out.println(i);
+            System.out.println(i);//prints out numbers
         }
     }
 }
